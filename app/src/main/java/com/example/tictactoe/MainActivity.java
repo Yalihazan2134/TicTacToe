23ae9d451343a5324262ba0303eb88961e1d7dc2
Yalihazan2134 <omriregev333@gmail.com>
2024-11-24 15:44:27 +0200

    xoxo final

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -8,52 +8,76 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.content.DialogInterface;
-import androidx.appcompat.app.AlertDialog;
-
+import android.app.AlertDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     String turn;
-    String [][] board;
+    String[][] board;
     int count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btn_00), (v, insets) -> {
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        onNewGame();
     }
-
 
     private void onNewGame() {
         board = new String[3][3];
-        for (int row=0; row < 3; row++)
-            for (int col=0; col < 3; col++)
-                board[row][col] = new String();
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = "";
+            }
+        }
 
         turn = "X";
         count = 0;
     }
 
+    public void onButtonClick(View view) {
+        if (view.getId() == R.id.btn_00)
+            handleClick(0, 0, R.id.btn_00);
+        else if (view.getId() == R.id.btn_01)
+            handleClick(0, 1, R.id.btn_01);
+        else if (view.getId() == R.id.btn_00)
+            handleClick(0, 2, R.id.btn_00);
+        else if (view.getId() == R.id.btn_10)
+            handleClick(1, 0, R.id.btn_10);
+        else if (view.getId() == R.id.btn_11)
+            handleClick(1, 1, R.id.btn_11);
+        else if (view.getId() == R.id.btn_12)
+            handleClick(1, 2, R.id.btn_12);
+        else if (view.getId() == R.id.btn_20)
+            handleClick(2, 0, R.id.btn_20);
+        else if (view.getId() == R.id.btn_21)
+            handleClick(2, 1, R.id.btn_21);
+        else if (view.getId() == R.id.btn_22)
+            handleClick(2, 2, R.id.btn_22);
+    }
+
     private void handleClick(int row, int col, int id) {
-        if (board[row][col].equals("")) {
+        if (board[row][col].isEmpty()) {
             board[row][col] = turn;
             Button btn = findViewById(id);
             btn.setText(turn);
             onTurnEnd();
         }
     }
+
     private void onTurnEnd() {
         if (isWinner())
             endGame(turn + " won!");
         else {
             count++;
-
             if (count == 9)
                 endGame("Tie");
             else {
@@ -62,67 +86,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void endGame(String s) {
+    private void endGame(String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("More Info");
-        String msg = "This is the message body";
-        builder.setMessage(msg);
-        builder.setPositiveButton("EXIT", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+        builder.setTitle("Game Over");
+        builder.setMessage(message);
 
-            }
+
+        builder.setPositiveButton("EXIT", (dialogInterface, i) -> {
+
         });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
 
-            }
-        });
-        AlertDialog dialog = builder.show();
-
+        builder.setNegativeButton("Play Again", (dialogInterface, i) -> onNewGame());
+        builder.show();
     }
 
     private boolean isWinner() {
-        for (int row = 0; row < 3; row++) {
-            if (board[row][0].equals(board[row][1]) && board[row][1].equals(board[row][2]) && !board[row][0].equals(""))
+
+        for (int i = 0; i < 3; i++) {
+            if (!board[i][0].isEmpty() && board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2])) {
                 return true;
+            }
         }
-        for (int col = 0; col < 3; col++) {
-            if (board[0][col].equals(board[1][col]) && board[1][col].equals(board[2][col]) && !board[0][col].equals(""))
+
+
+        for (int i = 0; i < 3; i++) {
+            if (!board[0][i].isEmpty() && board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i])) {
                 return true;
+            }
         }
-        if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals(""))
+
+
+        if (!board[0][0].isEmpty() && board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2])) {
             return true;
-        if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[0][2].equals(""))
-            return true;
-        return false;
-    }
-
-
-
-    public void onButtonClick(View view)
-        {
-            if (view.getId() == R.id.btn_00)
-                handleClick(0, 0, R.id.btn_00);
-            if (view.getId() == R.id.btn_01)
-                handleClick(0, 1, R.id.btn_01);
-            if (view.getId() == R.id.btn_02)
-                handleClick(0, 2, R.id.btn_02);
-            if (view.getId() == R.id.btn_10)
-                handleClick(1, 0, R.id.btn_10);
-            if (view.getId() == R.id.btn_11)
-                handleClick(1, 1, R.id.btn_11);
-            if (view.getId() == R.id.btn_12)
-                handleClick(1, 2, R.id.btn_12);
-            if (view.getId() == R.id.btn_20)
-                handleClick(2, 0, R.id.btn_20);
-
-            if (view.getId() == R.id.btn_21)
-                handleClick(2, 1, R.id.btn_21);
-            if (view.getId() == R.id.btn_22)
-                handleClick(2, 0, R.id.btn_22);
-
-
         }
+        return !board[0][2].isEmpty() && board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]);
     }
+}
